2f22e4b6622e3e36ebb92c5e52ed8e9969961c8b
misha <misha_zakharov96@mail.ru>
2019-04-12 00:54:12 +0300

    solved 17 problem

--- question17.py
+++ question17.py
@@ -1,16 +1,55 @@
 import inflect
 
 result = 0
+
 p = inflect.engine()
-for number in range(1,101):
+for number in range(1,1000):
+	i = ''
 	time = p.number_to_words(number)
-	if len(time) > 8:
-		result += len(time) - 1
-	else:
+	if number <= 20:
 		result += len(time)
+	elif number == 30:
+		result += len(time)
+	elif number == 40:
+		result += len(time)
+	elif number == 50:
+		result += len(time)
+	elif number == 60:
+		result += len(time)
+	elif number == 70:
+		result += len(time)
+	elif number == 80:
+		result += len(time)
+	elif number == 90:
+		result += len(time)
+	elif number == 100:
+		result += len(time) - 1
+	elif number > 20 and number < 100:
+		result += len(time) - 1
+	elif number > 100:
+		i = str(number)[0]
+		if number > int(i + '00') and number <= int(i + '20'):
+			result += len(time) - 3
+		elif number == int(i + '00'):
+			result += len(time) - 1
+		elif number == int(i + '30'):
+			result +=  len(time) - 3
+		elif number == int(i + '40'):
+			result += len(time) - 3
+		elif number == int(i + '50'):
+			result += len(time) - 3
+		elif number == int(i + '60'):
+			result += len(time) - 3
+		elif number == int(i + '70'):
+			result += len(time) - 3
+		elif number == int(i + '80'):
+			result += len(time) - 3
+		elif number == int(i + '90'):
+			result += len(time) - 3
+		else:
+			result += len(time) - 4
 
+print(result+11)
 
-print(result)
-
-
+# brainless solving
 
