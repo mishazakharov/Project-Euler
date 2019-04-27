0aec1fe546e08cda1a29e6e8ce0676fb9315a0fb
misha <misha_zakharov96@mail.ru>
2019-04-28 00:08:49 +0300

    19

--- question19.py
+++ question19.py
@@ -1,11 +1,6 @@
 '''
 Допустим 7ое числа января 1900 года - это воскресенье, тогда:
-
-i= 1 - понедельник
-i= 2 - вторник
-i= 7 - воскресенье
 '''
-
 casual_year = {
 			'January':31,
 			'February':28,
@@ -22,28 +17,32 @@ casual_year = {
 			}
 
 beginning_year = 1901
+needed_days = []
+# i - переменная, хранящая информацию о дне недели:
+i = 1
 
-for year in range(beginning_year,2001):
-	# Если год - високосный
-	if year%4 == 0:
-		for month in casual_year.keys():
+for year in range(beginning_year,2000):
+	for month in casual_year.keys():
+		# Если год - високосный
+		if year%4 == 0:
 			# Для февраля добавляется один день
 			# это единственный способ, до которого я додумался сейчас!
 			if month == 'February':
 				for day in range(casual_year[month]+1):
-					pass
+					if day == 0 and i == 7:
+						needed_days.append(day)
+					i += 1
+					if i == 8:
+						i = 1
 				continue
-			for day in range(casual_year[month]):
-				pass
-	# Если год обычный
-	else:
-		for month in casual_year.keys():
-			# i - переменная, хранящая информацию о дне недели:
-			i = 1
-			for day in range(casual_year[month]):
-				if day%(8-i) == 0:
-					print(day)
+		for day in range(casual_year[month]):
+			# Добавляет только воскресенья, с которых начинается месяц....
+			if day == 0 and i == 7:
+				needed_days.append(day)
+			# Обновление дней недели
+			i += 1
+			# С понедельника по воскресенье
+			if i == 8:
+				i = 1
 
-
-
-
+print(len(needed_days))
